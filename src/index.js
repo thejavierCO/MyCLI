f434edf68e483575b599f54065d01dc4d3792e89
javier cruz <eljaviercruz@outlook.com>
2020-10-29 23:28:20 -0600

    msg: change organizer

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ let setCapitaleDirectory  = (root,a)=>{
     return path.join(root,b+(r.slice(1)));
 }
 
-let getDirectory = async (dir,message,invert=false)=>existdir(dir)
+let getDirectory = async (dir,message,invert=false)=>await existdir(dir)
 .then(e=>{if(e){return dir;}else{throw dir;}})
 .then(e=>typeof message !== "undefined"&&invert===true?
     inquirer.prompt({
@@ -45,25 +45,12 @@ let run = async (a)=>{
             cwd:a
         });
     }
-    getDirectory(path.join(a,"src"),"not exist source directory \n your create?");
+    await getDirectory(path.join(a,"src"),"not exist source directory \n your create?");
     await cmd.command("code "+a);
 }
 
-if(typeof commands.get("new") !== "string"){
-    inquirer.prompt([
-        {
-            type:"input",
-            name:"Directory",
-            message:"Type Proyect is",
-            validate:a=>a===''?((a)=>{
-                console.log("require type proyect");
-                return a;
-            })(false):true
-        }
-    ])
-    .then(({Directory})=>setCapitaleDirectory(home,Directory))
-    .then(e=>getDirectory(e,"not exist your type directory"))
-    .then(dir=>
+let setProyect = async (a)=>{
+    await getDirectory(a,"not exist your type directory").then(dir=>
         inquirer.prompt([{
             type:"input",
             name:"project",
@@ -81,27 +68,26 @@ if(typeof commands.get("new") !== "string"){
             console.log(e,"ashdiua");
         }
     })
-}else{
-    if(commands.get("new")!==""){
-        let dir = setCapitaleDirectory(home,commands.get("new")); 
-        inquirer.prompt([{
+}
+
+if(typeof commands.get("new") !== "string"){
+    inquirer.prompt([
+        {
             type:"input",
-            name:"project",
-            message:"your name proyect is",
+            name:"Directory",
+            message:"Type Proyect is",
             validate:a=>a===''?((a)=>{
-                console.log("require name proyect");
+                console.log("require type proyect");
                 return a;
             })(false):true
-        }])
-        .then(({project})=>path.join(dir,project))
-        .then(e=>getDirectory(e,"exist directory continue",true))
-        .then(e=>run(e))
-        .catch(e=>{
-            let {result} = e;
-            if(result!==false){
-                console.log(e,"ashdiua");
-            }
-        })
+        }
+    ])
+    .then(({Directory})=>setCapitaleDirectory(home,Directory))
+    .then(e=>setProyect(e))
+}else if(commands.exists("new")){
+    if(commands.get("new")!==""){
+        let dir = setCapitaleDirectory(home,commands.get("new")); 
+        setProyect(dir)
     }else{
         throw "require name type proyect";
     }
