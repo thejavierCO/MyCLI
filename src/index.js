73b1708dbf6d06062fbdca230b7ce19d92cde146
javier cruz <eljaviercruz@outlook.com>
2020-10-29 22:43:34 -0600

    msg: is all good

---
+++ src/index.js
@@ -0,0 +1,104 @@
+let path = require("path");
+let cmd = require("execa");
+let commands = require("commands");
+let inquirer = require("inquirer");
+let {dirExists:existdir} = require("dir-exists-safe");
+
+let file = require("file-maker");
+
+let setDir = require("make-dir");
+
+let home = path.resolve("f:","Projects");
+
+let setCapitaleDirectory  = (root,a)=>{
+    let r = a.split("\\");
+        r = (r.slice(r.length-1))[0];
+    let b = (r[0].charAt(0)).toLocaleUpperCase();
+    return path.join(root,b+(r.slice(1)));
+}
+
+let getDirectory = async (dir,message,invert=false)=>existdir(dir)
+.then(e=>{if(e){return dir;}else{throw dir;}})
+.then(e=>typeof message !== "undefined"&&invert===true?
+    inquirer.prompt({
+        type:"confirm",
+        name:"exist",
+        message
+    }).then(({exist})=>exist?dir:false)
+:e)
+.catch(d=>typeof message !== "undefined"&&invert===false?
+    inquirer.prompt({
+        type:"confirm",
+        name:"make",
+        message
+    }).then(({make})=>make?createDir(d):false)
+:createDir(d))
+.then(e=>{if(e===false){throw {result:e};}else{return e;}})
+
+let createDir = async (e)=>e?setDir(e):false;
+
+let run = async (a)=>{
+    let test = await cmd.command("node",["init","-y"],{
+        stdio:"inherit",
+        cwd:a
+    });
+    console.log(test)
+}
+
+if(typeof commands.get("new") !== "string"){
+    inquirer.prompt([
+        {
+            type:"input",
+            name:"Directory",
+            message:"Type Proyect is",
+            validate:a=>a===''?((a)=>{
+                console.log("require type proyect");
+                return a;
+            })(false):true
+        }
+    ])
+    .then(({Directory})=>setCapitaleDirectory(home,Directory))
+    .then(e=>getDirectory(e,"not exist your type directory"))
+    .then(dir=>
+        inquirer.prompt([{
+            type:"input",
+            name:"project",
+            message:"your name proyect is",
+            validate:a=>a===''?((a)=>{
+                console.log("require name proyect");
+                return a;
+            })(false):true
+        }]).then(({project})=>setCapitaleDirectory(dir,project))
+    ).then(e=>getDirectory(e,"exist directory continue",true))
+    .then(e=>run(e))
+    .catch(e=>{
+        let {result} = e;
+        if(result!==false){
+            console.log(e,"ashdiua");
+        }
+    })
+}else{
+    if(commands.get("new")!==""){
+        let dir = setCapitaleDirectory(home,commands.get("new")); 
+        inquirer.prompt([{
+            type:"input",
+            name:"project",
+            message:"your name proyect is",
+            validate:a=>a===''?((a)=>{
+                console.log("require name proyect");
+                return a;
+            })(false):true
+        }])
+        .then(({project})=>setCapitaleDirectory(dir,project))
+        .then(e=>getDirectory(e,"exist directory continue",true))
+        .then(e=>run(e))
+        .catch(e=>{
+            let {result} = e;
+            if(result!==false){
+                console.log(e,"ashdiua");
+            }
+        })
+    }else{
+        throw "require name type proyect";
+    }
+}
