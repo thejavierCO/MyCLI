951a4314fe6bc88d63caa4f97b2b15aa132842aa
javier cruz <javier.cruz.odispo@hotmail.com>
2020-11-11 11:03:24 -0600

    reorganized content

--- src/tools/setArguments.js
+++ src/tools/setArguments.js
@@ -1,26 +1,26 @@
-const commands = require("commands");
+// const commands = require("commands");
 
-class argument{
-    async get(argument){
-        if(typeof argument === "object"&&argument.length){
-            return argument.map(e=>{
-                let arg = e.toLowerCase();
-                return commands.exists(arg)?({
-                    name:arg,
-                    data:commands.get(arg),
-                    exists:true
-                }):({
-                    name:arg,
-                    data:false,
-                    exists:false
-                });
-            })
-        }if(typeof argument === "string"){
-            return this.get([argument]).then(e=>e[0])
-        }else{
-            throw "not is object or array"
-        }
-    }
-}
+// class argument{
+//     async get(argument){
+//         if(typeof argument === "object"&&argument.length){
+//             return argument.map(e=>{
+//                 let arg = e.toLowerCase();
+//                 return commands.exists(arg)?({
+//                     name:arg,
+//                     data:commands.get(arg),
+//                     exists:true
+//                 }):({
+//                     name:arg,
+//                     data:false,
+//                     exists:false
+//                 });
+//             })
+//         }if(typeof argument === "string"){
+//             return this.get([argument]).then(e=>e[0])
+//         }else{
+//             throw "not is object or array"
+//         }
+//     }
+// }
 
-module.exports = argument;
+// module.exports = argument;
--- src/tools/setProyects.js
+++ src/tools/setProyects.js
@@ -1,10 +1,1 @@
-const inquirer = require("inquirer");
-const axios = require("axios");
-const nodeset = require("./nodeSet");
 const getGitignore = "https://www.toptal.com/developers/gitignore/api/node";
-
-class setProyect{
-    
-}
-
-module.exports = setProyect
