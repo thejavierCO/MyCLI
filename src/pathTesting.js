ed34fe579eb053dd769fb5c0ec9a9ce5b6fc2138
javier cruz <javier.cruz.odispo@hotmail.com>
2020-11-12 22:16:26 -0600

    set nodeManager and ProyectManager

--- src/pathTesting.js
+++ src/pathTesting.js
@@ -2,7 +2,5 @@ const Directory = require("./tools/ProyectManager");
 const Node = require("./tools/NodeManager");
 
 let PT = new Directory("f:/tools");
-
 let workdirectory = PT.Type.set("svelte");
-
-console.log(new Node(workdirectory.new("testing")),8)
+let nodetest = new Node(workdirectory.new("testing"));
--- src/tools/NodeManager/index.js
+++ src/tools/NodeManager/index.js
@@ -1,16 +1,21 @@
+const path = require("path")
+const fs = require("fs");
 const execa = require("execa");
 
 class NodeManager{
     constructor(projectManager){
-        let {type,root} = projectManager;
-        this.root = root;
-        this.control = {
-            setDirectory:projectManager.setDirectory,
-            getDirectory:projectManager.getDirectory,
-            delDirectory:projectManager.delDirectory,
-            readDirectory:projectManager.readDirectory,
+        this.project = projectManager;
+        this.init = async ()=>await execa("npm init -y",{stdout:"inherit",cwd:this.project.dir.root})
+        this.getModules = async (modules,isdev)=>{
+            let dev = isdev?" -D":" ";
+            if(fs.existsSync(path.resolve(this.project.dir.root,"package.json"))){
+                if(typeof modules === "object"&&modules.length>0){
+                    return await execa("npm i "+modules.join(" ")+dev,{stdout:"inherit",cwd:this.project.dir.root})
+                }
+            }else{
+                throw {error:"not set package.json",path:this.project.dir.root}
+            }
         }
-        this.init = async ()=>await execa("npm init -y",{stdout:"inherit",cwd:root})
     }
 }
 
