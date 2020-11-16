e151f9aa677d85ccbd3f9691171a31e43f84901a
javier cruz <javier.cruz.odispo@hotmail.com>
2020-11-16 11:26:37 -0600

    testing my class Quest

--- src/pathTesting.js
+++ src/pathTesting.js
@@ -1,45 +1,27 @@
-const quest = require("./tools/questManager")
-const Directory = require("./tools/ProyectManager");
-const Node = require("./tools/NodeManager");
-const Git = require("./tools/GitManager");
+const questManager = require("./tools/questManager");
 
-let dir = new Directory("f:/tools");
+let PT = new questManager("f:/tools")
 
-let CLI = new quest(dir);
-
-CLI.getType()
-.then(e=>
-    CLI.getProject(false,e).catch(e=>{
-        console.log(e)
-        throw e;
-    })
-)
-.then(e=>
-    CLI.setNode(false,e).catch(e=>{
-        console.log(e)
-        throw e;
-    })
-)
-.then(e=>
-    CLI.setinitNode(e).catch(e=>{
-        console.log(e)
-        throw e;
-    })
-)
-.then(e=>
-    CLI.setGit(false,e).catch(e=>{
-        console.log(e)
-        throw e;
-    })
-)
-.then(e=>
-    CLI.setinitGit(e).catch(e=>{
-        console.log(e)
-        throw e;
-    })
-)
-.then(e=>console.log(e))
-.catch(e=>{
-    console.log(e)
-    throw e;
+PT.Quest(
+    {
+        type:"confirm",
+        name:"new",
+        message:"Seguro que quiere crear un proyecto",
+        require:(a)=>a==true
+    },
+    {
+        type:"input",
+        name:"type",
+        message:"espesifique el typo de proyecto",
+        isImportant:true,
+        require:(a)=>/[A-Z]/i.test(a)
+    }
+).then(a=>{	
+    if(a.exit)return "";
+    return PT.WorkDirectory.Type.get(a.type);
+}).then(a=>{
+    // if(a.exit)return "";
+    // return PT.WorkDirectory.Type(a.type);
+    console.log(a);
 })
+.catch(e=>{console.log(e,21)})
--- src/tools/questManager/index.js
+++ src/tools/questManager/index.js
@@ -1,86 +1,189 @@
 const commands = require("commands")
+const inquirer = require("inquirer");
 const Node = require("../NodeManager")
 const Git = require("../GitManager");
-const inquirer = require("inquirer");
+const Directory = require("../ProyectManager");
+const { get } = require("commands");
+
+function getType(element){
+    return typeof element === "object"?element.length>0?"array":"object":typeof element;
+}
 
 class quest{
     constructor(PT){
-        this.dir = PT;
-    }
-    async getType(name=commands.get("type")){
-        let result;
-        if(typeof name === "string"){
-            result = name;
-        }else{
-            result = await inquirer.prompt({
-                type:"input",
-                name:"type",
-                message:"type is ..."
-            }).then(({type})=>{
-                if(type==="")throw {error:"not defined type"}
-                return type;
-            })
+        switch(getType(PT)){
+            case "object":
+                let {directory} = PT;
+                this.WorkDirectory = new Directory(directory);
+            break;
+            case "string":
+                this.WorkDirectory = new Directory(PT);
+            break;
+            default:
+                throw {error:"require object or string with directory"}
         }
-        return this.dir.Type.get(result);
-    }
-    async getProject(name=commands.get("name"),type){
-        if(name===false) name = commands.get("name");
-        let result;
-        if(typeof name === "string"){
-            result = name;
-        }else{
-            result = await inquirer.prompt({
-                type:"input",
-                name:"name",
-                message:"name Project is ..."
-            }).then(({name})=>{
-                if(name==="")throw {error:"not defined name"}
-                return name;
-            })
+        this.Quest = async (...arg)=>{
+            let result = [];
+            try{
+                for (let i = 0; i < arg.length;i++) {
+                    if(getType(arg[i])==="object"){
+                        let {
+                            name,
+                            isImportant,
+                            ImportantMsg,
+                            require
+                        } = arg[i];
+                        if(!name){
+                            throw {error:"require name"}
+                        }
+                        if(isImportant==true){
+                            isImportant = (a)=>a==""?(()=>{
+                                console.log(
+                                    ImportantMsg
+                                    &&
+                                    getType(ImportantMsg)==="string"?
+                                    "\n"+ImportantMsg
+                                    :"\nEs importante\nPor favor agregue algo")
+                                return false;
+                            })():true
+                        }else{
+                            isImportant = ()=>true;
+                        }
+                        if(commands.exists(name)){
+                            result.push((e=>this.require(arg[i],require,e))({[name]:commands.get(name)}))
+                        }else{
+                            result.push(await inquirer.prompt({
+                                ...arg[i],
+                                validate:isImportant
+                            }).then(e=>this.require(arg[i],require,e)))
+                        }
+                    }else{
+                        throw {error:"require object"}
+                    }
+                }
+                return result.length<=1?result[0]:this.setObject(result);
+            }catch(err){
+                if(err.message){
+                    return {exit:err}
+                }else{
+                    throw err;
+                }
+            }
         }
-        return type.get(result);
     }
-    async setNode(name=commands.get("node"),project){
-        if(name===false) name = commands.get("node");
-        let result;
-        if(typeof name === "boolean"){
-            result = name;
-        }else{
-            result = await inquirer.prompt({
-                type:"confirm",
-                name:"node",
-                message:"init node?"
-            }).then(({node})=>{
-                if(node===false)throw {error:"not set node"}
-                return node;
+    setObject(array){
+        if(getType(array)==="array"){
+            let result = {};
+            array.map(e=>{
+                let key = Object.keys(e);
+                if(result[key]){
+                    if(getType(result[key])!=="array"){
+                        const old = result[key];
+                        result[key] = [];
+                        result[key].push(old);
+                        result[key].push(e[key])
+                    }else if(getType(result[key])==="array"){
+                        result[key].push(e[key]);
+                    }else{
+                        console.log("warn:",e[key],key)
+                    }
+                }else{
+                    result[key] = e[key];
+                }
             })
-        }
-        return new Node(project);
-    }
-    async setinitNode(projectNode){
-        projectNode.init();
-        return projectNode.project;
-    }
-    async setGit(name=commands.get("git"),project){
-        if(name===false) name = commands.get("git");
-        let result;
-        if(typeof name === "boolean"){
-            result = name;
+            return result;
         }else{
-            result = await inquirer.prompt({
-                type:"confirm",
-                name:"git",
-                message:"init git?"
-            }).then(({git})=>{
-                if(git===false)throw {error:"not set git"}
-                return git;
-            })
+            throw {error:"require array"}
         }
-        return new Git(project);
     }
-    async setinitGit(projectGit){
-        return projectGit.init();
+    require(a,b,c){
+        if(getType(b)==="function"){
+            let {name} = a;
+            let validate = b(c[name]);
+            if(getType(validate)==="boolean"&&validate==false)throw {message:"element is false",data:c};
+            if(getType(validate)==="object"){
+                let {message,send} = validate;
+                if(send==false){
+                    throw {message,send}
+                }
+            }
+        }
+        return c;
     }
+    // async getType(name=commands.get("type")){
+    //     let result;
+    //     if(typeof name === "string"){
+    //         result = name;
+    //     }else{
+    //         result = await inquirer.prompt({
+    //             type:"input",
+    //             name:"type",
+    //             message:"type is ..."
+    //         }).then(({type})=>{
+    //             if(type==="")throw {error:"not defined type"}
+    //             return type;
+    //         })
+    //     }
+    //     return this.dir.Type.get(result);
+    // }
+    // async getProject(name=commands.get("name"),type){
+    //     if(name===false) name = commands.get("name");
+    //     let result;
+    //     if(typeof name === "string"){
+    //         result = name;
+    //     }else{
+    //         result = await inquirer.prompt({
+    //             type:"input",
+    //             name:"name",
+    //             message:"name Project is ..."
+    //         }).then(({name})=>{
+    //             if(name==="")throw {error:"not defined name"}
+    //             return name;
+    //         })
+    //     }
+    //     return type.get(result);
+    // }
+    // async setNode(name=commands.get("node"),project){
+    //     if(name===false) name = commands.get("node");
+    //     let result;
+    //     if(typeof name === "boolean"){
+    //         result = name;
+    //     }else{
+    //         result = await inquirer.prompt({
+    //             type:"confirm",
+    //             name:"node",
+    //             message:"init node?"
+    //         }).then(({node})=>{
+    //             if(node===false)throw {error:"not set node"}
+    //             return node;
+    //         })
+    //     }
+    //     return new Node(project);
+    // }
+    // async setinitNode(projectNode){
+    //     projectNode.init();
+    //     return projectNode.project;
+    // }
+    // async setGit(name=commands.get("git"),project){
+    //     if(name===false) name = commands.get("git");
+    //     let result;
+    //     if(typeof name === "boolean"){
+    //         result = name;
+    //     }else{
+    //         result = await inquirer.prompt({
+    //             type:"confirm",
+    //             name:"git",
+    //             message:"init git?"
+    //         }).then(({git})=>{
+    //             if(git===false)throw {error:"not set git"}
+    //             return git;
+    //         })
+    //     }
+    //     return new Git(project);
+    // }
+    // async setinitGit(projectGit){
+    //     return projectGit.init();
+    // }
 }
 
 module.exports = quest;
